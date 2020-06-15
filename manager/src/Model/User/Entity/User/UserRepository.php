317ccb88121ac964af5c8d1a086c3d95633b02b4
SEKarpov <sergo8ck@gmail.com>
2020-06-16 00:47:16 +0300

    Incapsulated responsibilities

---
+++ manager/src/Model/User/Entity/User/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+
+interface UserRepository
+{
+    public function hasByEmail(Email $email): bool;
+
+    public function add(User $user): void;
+}
