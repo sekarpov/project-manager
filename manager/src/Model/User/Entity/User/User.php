f1d64763f64944b42918554c07cfd39ecf5901e8
SEKarpov <sergo8ck@gmail.com>
2020-06-16 23:39:05 +0300

    Added sign up confirm

--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -50,6 +50,16 @@ class User
         $this->status = self::STATUS_WAIT;
     }
 
+    public function confirmSignUp(): void
+    {
+        if(!$this->isWait()){
+            throw new \DomainException('User is already confirmed.');
+        }
+
+        $this->status = self::STATUS_ACTIVE;
+        $this->confirmToken = null;
+    }
+
     public function isWait(): bool
     {
         return $this->status === self::STATUS_WAIT;
--- manager/src/Model/User/Entity/User/UserRepository.php
+++ manager/src/Model/User/Entity/User/UserRepository.php
@@ -7,6 +7,8 @@ namespace App\Model\User\Entity\User;
 
 interface UserRepository
 {
+    public function findByConfirmToken(string $token): ?User;
+
     public function hasByEmail(Email $email): bool;
 
     public function add(User $user): void;
