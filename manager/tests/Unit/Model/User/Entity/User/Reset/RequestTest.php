6a86455473344975808e6ad978682060c0e13954
SEKarpov <sergo8ck@gmail.com>
2020-06-19 00:11:34 +0300

    Added user active checking

--- manager/tests/Unit/Model/User/Entity/User/Reset/RequestTest.php
+++ manager/tests/Unit/Model/User/Entity/User/Reset/RequestTest.php
@@ -15,7 +15,7 @@ class RequestTest extends TestCase
         $now = new \DateTimeImmutable();
         $token = new ResetToken('token', $now->modify('+1 day'));
 
-        $user = (new UserBuilder())->viaEmail()->build();
+        $user = (new UserBuilder())->viaEmail()->confirmed()->build();
 
         $user->requestPasswordReset($token, $now);
 
@@ -27,7 +27,7 @@ class RequestTest extends TestCase
         $now = new \DateTimeImmutable();
         $token = new ResetToken('token', $now->modify('+1 day'));
 
-        $user = (new UserBuilder())->viaEmail()->build();
+        $user = (new UserBuilder())->viaEmail()->confirmed()->build();
         $user->requestPasswordReset($token, $now);
 
         $this->expectExceptionMessage('Resetting is already requested.');
@@ -38,7 +38,7 @@ class RequestTest extends TestCase
     {
         $now = new \DateTimeImmutable();
 
-        $user = (new UserBuilder())->viaEmail()->build();
+        $user = (new UserBuilder())->viaEmail()->confirmed()->build();
 
         $token1 = new ResetToken('token', $now->modify('+1 day'));
         $user->requestPasswordReset($token1, $now);
@@ -49,6 +49,17 @@ class RequestTest extends TestCase
         $user->requestPasswordReset($token2, $now->modify('+2 day'));
 
         self::assertEquals($token2, $user->getResetToken());
+    }
+
+    public function testNotConfirmed(): void
+    {
+        $now = new \DateTimeImmutable();
+        $token = new ResetToken('token', $now->modify('+1 day'));
+
+        $user = (new UserBuilder())->viaEmail()->build();
+
+        $this->expectExceptionMessage('User is not active.');
+        $user->requestPasswordReset($token, $now);
     }
 
     public function testWithoutEmail(): void
