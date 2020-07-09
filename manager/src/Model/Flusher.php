438c7b07cdd6cf3397a9b9bc6af91e6d41b5199f
SEKarpov <sergo8ck@gmail.com>
2020-07-09 17:58:08 +0300

    Implemented repository

--- manager/src/Model/Flusher.php
+++ manager/src/Model/Flusher.php
@@ -3,8 +3,19 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use Doctrine\ORM\EntityManagerInterface;
 
-interface Flusher
+class Flusher
 {
-    public function flush(): void;
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function flush(): void
+    {
+        $this->em->flush();
+    }
 }
